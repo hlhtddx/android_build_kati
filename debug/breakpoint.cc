9b88f8c5a8455982f4b9646350e81c0a88330fdb
Frank Zhang <frankzhang123@gmail.com>
2019-02-06 16:32:40 +0800

    Add KATI_DEBUG_* flags for embedded kati running
    For ckati is commonly launched from soong_ui while Android is being
    built, we cannot manually run it in console. Thus, we add a new env
    variable KATI_DEBUG_FLAG to control whether to start debug session,
    and KATI_DEBUG_CLIENT for client connection config.
    
    Change-Id: I4cf3a40b14c840c30311fbae2a22b48448a59e7f

--- debug/breakpoint.cc
+++ debug/breakpoint.cc
@@ -19,159 +19,132 @@ namespace Debug {
   class BrokenFileMap : public unordered_map<string, BrokenFile> {
    public:
     //Insert a new break point. Do nothing if exists
-    void Insert(const char *path, int lineno);
+    void Insert(const char *path, int lineno) {
+      string real_path(path);
+      auto iter = find(real_path);
+      if (iter == end()) {
+        emplace(real_path, BrokenFile());
+      }
+      iter = find(real_path);
+      iter->second.insert(lineno);
+    }
 
     //Remove a break point. Do nothing if not exist
-    void Remove(const string &path, int lineno);
+    void Remove(const string &path, int lineno) {
+      auto iter = find(path);
+      if (iter == end()) {
+        return;
+      }
+      auto &bp_file = iter->second;
+      bp_file.erase(lineno);
+      if (bp_file.empty()) {
+        erase(path);
+      }
+    }
 
     //Remove all break points. Do nothing if not exist
-    void RemoveAll();
+    void RemoveAll() {
+      clear();
+    }
 
     //Return true if exists, or false otherwise
-    bool Find(const string &path, int lineno) const;
+    bool Find(const string &path, int lineno) const {
+      auto iter = find(path);
+      if (iter == end()) {
+        return false;
+      }
+      auto &bp_file = iter->second;
+      return bp_file.Find(lineno);
+    }
 
     //Return true if exists, or false otherwise
-    const BrokenFile *Find(const string &path) const;
+    const BrokenFile *Find(const string &path) const {
+      if (path.empty()) {
+        return nullptr;
+      }
+      auto iter = find(path);
+      if (iter == end()) {
+        return nullptr;
+      }
+      return &(iter->second);
+    }
   };
-
-  void BrokenFileMap::Insert(const char *path, int lineno) {
-    string real_path(path);
-    auto iter = find(real_path);
-    if (iter == end()) {
-      emplace(real_path, BrokenFile());
-    }
-    iter = find(real_path);
-    iter->second.insert(lineno);
-  }
-
-  void BrokenFileMap::Remove(const string& path, int lineno) {
-    auto iter = find(path);
-    if (iter == end()) {
-      return;
-    }
-    auto &bp_file = iter->second;
-    bp_file.erase(lineno);
-    if (bp_file.empty()) {
-      erase(path);
-    }
-  }
 
   class BreakPointList : public vector<BreakPoint> {
    public:
     //Insert a new break point. Do nothing if exists
-    void Insert(const char *path, int lineno);
+    void Insert(const char *path, int lineno) {
+      emplace_back(path, lineno);
+      sort(begin(), end(), BreakPoint::Compare);
+    }
 
     //Remove a break point. Do nothing if not exist
-    void Remove(int index);
+    void Remove(int index) {
+      erase(begin() +index);
+    }
 
     //Remove all break points. Do nothing if not exist
-    void RemoveAll();
+    void RemoveAll() {
+      clear();
+    }
   };
-
-  void BrokenFileMap::RemoveAll() {
-    clear();
-  }
-
-  bool BrokenFileMap::Find(const string& path, int lineno) const {
-    auto iter = find(path);
-    if (iter == end()) {
-      return false;
-    }
-    auto &bp_file = iter->second;
-    return bp_file.Find(lineno);
-  }
-
-  const BrokenFile* BrokenFileMap::Find(const string& path) const {
-    auto iter = find(path);
-    if (iter == end()) {
-      return nullptr;
-    }
-    return &(iter->second);
-  }
-
-  void BreakPointList::Insert(const char *path, int lineno) {
-    emplace_back(path, lineno);
-    sort(begin(), end(), BreakPoint::Compare);
-  }
-
-  void BreakPointList::Remove(int index) {
-    erase(begin() +index);
-  }
-
-  void BreakPointList::RemoveAll() {
-    clear();
-  }
 
   class BreakPointsImpl : public BreakPoints {
    public:
-    BreakPointsImpl();
+    BreakPointsImpl() : current_bf_(nullptr) {
+    }
     //Insert a new break point. Do nothing if exists
-    void Insert(const char *path, int lineno) override;
+    void Insert(const char *path, int lineno) override {
+      if (bf_map_.Find(path, lineno)) {
+        WARN("Break point at %s:%d already exists!\n", path, lineno);
+        return;
+      }
+      bf_map_.Insert(path, lineno);
+      bp_list_.Insert(path, lineno);
+    }
 
     //Remove a break point. Do nothing if not exist
-    bool Remove(int index) override;
+    bool Remove(int index) override {
+      if (index < 0 || index >= bp_list_.size()) {
+        WARN("Break point index %d does not exist!\n", index + 1);
+        return false;
+      }
+      auto& bp = bp_list_[index];
+      bf_map_.Remove(bp.filename, bp.lineno);
+      bp_list_.Remove(index);
+      return true;
+    }
+
 
     //Remove all break points. Do nothing if not exist
-    void RemoveAll() override;
+    void RemoveAll() override {
+      bf_map_.RemoveAll();
+      bp_list_.RemoveAll();
+    }
 
     //Remove a vector of all break points.
-    const vector<BreakPoint>& GetList() override;
+    const vector<BreakPoint>& GetList() override {
+      return bp_list_;
+    }
 
     //Set current file that is being parsed, to check the break point
-    void SetCurrentFile(const string &path) override;
+    void SetCurrentFile(const string &path) override {
+      current_bf_ = bf_map_.Find(path);
+    }
 
     //Return true if it should be broken at current line
-    bool ShouldBreak(int lineno) override;
+    bool ShouldBreak(int lineno) override {
+      if (current_bf_ == nullptr) {
+        return false;
+      }
+      return current_bf_->Find(lineno);
+    }
 
    private:
     const BrokenFile *current_bf_;
     BreakPointList bp_list_;
     BrokenFileMap bf_map_;
   };
-
-  BreakPointsImpl::BreakPointsImpl()
-    : current_bf_(nullptr) {
-  }
-
-  void BreakPointsImpl::Insert(const char *path, int lineno) {
-    if (bf_map_.Find(path, lineno)) {
-      WARN("Break point at %s:%d already exists!\n", path, lineno);
-      return;
-    }
-    bf_map_.Insert(path, lineno);
-    bp_list_.Insert(path, lineno);
-  }
-
-  bool BreakPointsImpl::Remove(int index) {
-    if (index < 0 || index >= bp_list_.size()) {
-      WARN("Break point index %d does not exist!\n", index + 1);
-      return false;
-    }
-    auto& bp = bp_list_[index];
-    bf_map_.Remove(bp.filename, bp.lineno);
-    bp_list_.Remove(index);
-    return true;
-  }
-
-  void BreakPointsImpl::RemoveAll() {
-    bf_map_.RemoveAll();
-    bp_list_.RemoveAll();
-  }
-
-  const vector<BreakPoint>& BreakPointsImpl::GetList() {
-    return bp_list_;
-  }
-
-  void BreakPointsImpl::SetCurrentFile(const string& path) {
-    current_bf_ = bf_map_.Find(path);
-  }
-
-  bool BreakPointsImpl::ShouldBreak(int lineno) {
-    if (current_bf_ == nullptr) {
-      return false;
-    }
-    return current_bf_->Find(lineno);
-  }
 
   unique_ptr<BreakPoints> GetBreakPoints() {
     return make_unique<BreakPointsImpl>();
---
+++ debug/connector.cc
@@ -0,0 +1,149 @@
+//
+// Created by 张航 on 2019-02-06.
+//
+
+#include <regex>
+using namespace std;
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "command_interface.h"
+#include "log.h"
+
+namespace Debug {
+  class DefaultConnector : public Connector {
+   public:
+    DefaultConnector() = default;
+
+    virtual ~DefaultConnector() = default;
+
+    bool WaitForConnection() override {
+      // do nothing for console is always connected
+      return true;
+    }
+
+    void OnResponse(const Message &response) override {
+      response.Write(GetOutputHandle());
+    }
+
+    int GetInputHandle() override {
+      return fileno(stdin);
+    }
+
+    int GetOutputHandle() override {
+      return fileno(stdout);
+    }
+  };
+
+  class SocketConnector : public Connector {
+   public:
+    explicit SocketConnector()
+      : fd_(-1), listen_fd_(-1) {
+    }
+
+    virtual ~SocketConnector() {
+      if (fd_ != -1){
+        close(fd_);
+      }
+      if (listen_fd_ != -1){
+        close(listen_fd_);
+      }
+    };
+
+    bool WaitForConnection() override {
+      WARN("Waiting for connection");
+      while(listen_fd_ == -1) {
+        listen_fd_ = CreateSocket();
+      }
+      sockaddr_in client_addr = {0};
+      socklen_t client_addr_len = sizeof(client_addr);
+      do {
+        fd_ = accept(listen_fd_, reinterpret_cast<sockaddr *>(&client_addr), &client_addr_len);
+      } while (fd_ == -1);
+      return true;
+    }
+
+    void OnResponse(const Message &response) override {
+      response.Write(GetOutputHandle());
+    }
+
+    int GetInputHandle() override {
+      return fd_;
+    }
+
+    int GetOutputHandle() override {
+      return fd_;
+    }
+
+   protected:
+    virtual int CreateSocket() = 0;
+
+    int fd_;
+    int listen_fd_;
+  };
+
+  class TcpSocketConnector : public SocketConnector {
+   public:
+    explicit TcpSocketConnector(int port) : port_(port) {
+    }
+
+   protected:
+    int CreateSocket() override {
+
+      /* First call to socket() function */
+      listen_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+
+      if (listen_fd_ < 0) {
+        return -1;
+      }
+
+      sockaddr_in server_addr = {
+          .sin_family = AF_INET,
+          .sin_addr.s_addr = INADDR_ANY,
+          .sin_port = htons(port_),
+      };
+
+      if (::bind(listen_fd_, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        close(listen_fd_);
+        return -1;
+      }
+
+      /* Now start listening for the clients, here
+         * process will go in sleep mode and will wait
+         * for the incoming connection
+      */
+
+      if (listen(listen_fd_, 1) < 0) {
+        close(listen_fd_);
+        return -1;
+      }
+      return listen_fd_;
+    }
+   private:
+    int port_;
+  };
+
+
+  regex patterns[] = {
+      regex(R"(tcp:(\d+))"),
+      regex(R"(unix:(\w+))"),
+      regex(R"(pipe:(\w+))"),
+  };
+
+  Connector *GetConnector(const char *connector) {
+    if (connector == nullptr) {
+      return new DefaultConnector;
+    }
+    string connect(connector);
+    smatch matched;
+    if (regex_match(connect, matched, patterns[0])) {
+      int port = stoi(matched[1].str());
+      return new TcpSocketConnector(port);
+    }
+
+    return new DefaultConnector;
+  }
+
+}
